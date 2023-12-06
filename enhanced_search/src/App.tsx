427ea0803ab924fea205c718568e5576790b4d47
rdestefa <360fanelite@gmail.com>
2023-12-06 01:19:28 -0800

    Added Navigation Buttons and Keyboard Shortcuts

--- enhanced_search/src/App.tsx
+++ enhanced_search/src/App.tsx
@@ -1,8 +1,11 @@
-import { useState, useEffect, useMemo } from 'react';
+import {useState, useEffect, useMemo} from 'react';
 import Paper from '@mui/material/Paper';
 import InputAdornment from '@mui/material/InputAdornment';
 import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import debounce from 'lodash/debounce';
 
@@ -13,10 +16,10 @@ const App: React.FC = () => {
 
   useEffect(() => {
     async function insertHighlightCSS() {
-      const currentTabId = (await chrome.tabs.query({ active: true}))?.[0]?.id;
+      const currentTabId = (await chrome.tabs.query({active: true}))?.[0]?.id;
       chrome.scripting.insertCSS({
         css: 'mark.current-match { background-color: #FFB300 }',
-        target: { tabId: currentTabId ?? -1 },
+        target: {tabId: currentTabId ?? -1},
       });
     }
 
@@ -25,11 +28,13 @@ const App: React.FC = () => {
 
   useEffect(() => {
     async function getPageContent() {
-      const currentTabId = (await chrome.tabs.query({ active: true}))?.[0]?.id;
-      const data = (await chrome.scripting.executeScript({
-        func: () => document.body.innerText,
-        target: { tabId: currentTabId ?? -1 },
-      }))?.[0]?.result;
+      const currentTabId = (await chrome.tabs.query({active: true}))?.[0]?.id;
+      const data = (
+        await chrome.scripting.executeScript({
+          func: () => document.body.innerText,
+          target: {tabId: currentTabId ?? -1},
+        })
+      )?.[0]?.result;
       setPageContent(data);
     }
 
@@ -38,10 +43,11 @@ const App: React.FC = () => {
 
   useEffect(() => {
     async function highlightWord() {
-      const currentTabId = (await chrome.tabs.query({ active: true}))?.[0]?.id;
+      const currentTabId = (await chrome.tabs.query({active: true}))?.[0]?.id;
       chrome.scripting.executeScript({
-        func: (searchText, currentMatch) => doHighlight(searchText, currentMatch),
-        target: { tabId: currentTabId ?? -1 },
+        func: (searchText, currentMatch) =>
+          doHighlight(searchText, currentMatch),
+        target: {tabId: currentTabId ?? -1},
         // @ts-ignore (TS insists args has to be empty)
         args: [searchText, currentMatch],
       });
@@ -51,13 +57,43 @@ const App: React.FC = () => {
   }, [searchText, currentMatch]);
 
   const totalMatches = useMemo(() => {
-    return pageContent.match(new RegExp(searchText, 'gi'))?.length ?? 0;
+    return searchText
+      ? pageContent.match(new RegExp(searchText, 'gi'))?.length ?? 0
+      : 0;
   }, [pageContent, searchText]);
 
+  const incrementMatch = () => {
+    if (currentMatch === totalMatches - 1) {
+      setCurrentMatch(0);
+    } else {
+      setCurrentMatch(currentMatch + 1);
+    }
+  };
+
+  const decrementMatch = () => {
+    if (currentMatch === 0) {
+      setCurrentMatch(totalMatches - 1);
+    } else {
+      setCurrentMatch(currentMatch - 1);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLFormElement>) => {
+    if (totalMatches > 0) {
+      if (event.code === 'Enter' && event.shiftKey) {
+        decrementMatch();
+      } else if (event.code === 'Enter') {
+        incrementMatch();
+      }
+    }
+  };
 
   return (
     <Paper
       component="form"
+      onKeyDown={handleKeyDown}
+      // Otherwise page will refresh when "Enter" is pressed.
+      onSubmit={(event) => event.preventDefault()}
       sx={{
         p: '2px 4px',
         display: 'flex',
@@ -70,15 +106,31 @@ const App: React.FC = () => {
         sx={{ml: 1, mr: 1.5, flex: 1}}
         placeholder="Search"
         inputProps={{'aria-label': 'search'}}
-        multiline={true}
-        maxRows={5}
         onChange={debounce((e) => setSearchText(e.target.value), 500)}
         endAdornment={
-          searchText && (
-            <InputAdornment position="end">
-              {currentMatch}/{totalMatches}
-            </InputAdornment>
-          )
+          <InputAdornment position="end">
+            {searchText && (
+              <span>
+                {totalMatches > 0 ? currentMatch + 1 : 0}/{totalMatches}
+              </span>
+            )}
+            <IconButton
+              aria-label="previous-search-result"
+              size="small"
+              edge="end"
+              disabled={totalMatches <= 0}
+              onClick={decrementMatch}>
+              <KeyboardArrowUpIcon />
+            </IconButton>
+            <IconButton
+              aria-label="next-search-result"
+              size="small"
+              edge="start"
+              disabled={totalMatches <= 0}
+              onClick={incrementMatch}>
+              <KeyboardArrowDownIcon />
+            </IconButton>
+          </InputAdornment>
         }
       />
     </Paper>
